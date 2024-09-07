25d1542fe869104982aa58744fa6c0533667c07b
Trx211 <021kevinp@gmail.com>
2024-09-07 15:33:55 -0500

    CLase Membresia full

---
+++ Membresia.cpp
@@ -0,0 +1,82 @@
+#include "Membresia.h"
+
+Membresia::Membresia(int _idMembresia, string _tipo, double _costo, int _duracionDias, string _fechaInicio, string _fechaFin)
+{
+    //ctor
+
+}
+
+Membresia::~Membresia()
+{
+    //dtor
+    cout << "Membresía ID " << idMembresia << " eliminada." << endl;
+}
+
+
+
+int Membresia::getIdMembresia(){
+    return idMembresia;
+}
+
+string Membresia::getTipo(){
+    return tipo;
+}
+
+double Membresia::getCosto(){
+    return costo;
+}
+
+int Membresia::getDuracionDias(){
+    return duracionDias;
+}
+
+string Membresia::getFechaInicio(){
+    return fechaInicio;
+}
+
+string Membresia::getFechaFin(){
+    return fechaFin;
+}
+
+void Membresia::setTipo(string _tipo){
+    tipo= _tipo;
+}
+
+void Membresia::setCosto(double _costo){
+    costo= _costo;
+}
+
+void Membresia::setDuracionDias(int _duracionDias){
+    duracionDias= _duracionDias;
+}
+
+void Membresia::setFechaInicio(string _fechaInicio){
+    fechaInicio= _fechaInicio;
+}
+
+void Membresia::setFechaFin(string _fechaFin){
+    fechaFin= _fechaFin;
+}
+
+
+void Membresia::mostrarInformacion(){
+    cout << "Membresía ID: " << idMembresia << endl;
+    cout << "Tipo: " << tipo << endl;
+    cout << "Costo: $" << costo << endl;
+    cout << "Duración: " << duracionDias << " días" << endl;
+    cout << "Fecha de inicio: " << fechaInicio << endl;
+    cout << "Fecha de finalización: " << fechaFin << endl;
+}
+
+void Membresia::modificarMembresia(string nuevoTipo, double nuevoCosto, int nuevaDuracion, string nuevaFechaInicio, string nuevaFechaFin) {
+    setTipo(nuevoTipo);
+    setCosto(nuevoCosto);
+    setDuracionDias(nuevaDuracion);
+    setFechaInicio(nuevaFechaInicio);
+    setFechaFin(nuevaFechaFin);
+    cout << "Membresía modificada. Nuevo tipo: " << nuevoTipo
+        << ", Nuevo costo: $" << nuevoCosto
+        << ", Nueva duración: " << nuevaDuracion << " días"
+        << ", Nueva fecha de inicio: " << nuevaFechaInicio
+        << ", Nueva fecha de finalización: " << nuevaFechaFin << endl;
+}
---
+++ Membresia.h
@@ -0,0 +1,46 @@
+#ifndef MEMBRESIA_H
+#define MEMBRESIA_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Clase Membresia
+class Membresia {
+private:
+    int idMembresia;          // Identificador único de la membresía
+    string tipo;              // Tipo de membresía (Mensual, Anual, VIP, etc.)
+    double costo;             // Costo de la membresía
+    int duracionDias;         // Duración de la membresía en días
+    string fechaInicio;       // Fecha de inicio de la membresía
+    string fechaFin;          // Fecha de finalización de la membresía
+
+public:
+    // Constructor
+    Membresia(int _idMembresia, string _tipo, double _costo, int _duracionDias, string _fechaInicio, string _fechaFin);
+    virtual ~Membresia();
+
+    // Métodos getters
+    int getIdMembresia();
+    string getTipo();
+    double getCosto();
+    int getDuracionDias();
+    string getFechaInicio();
+    string getFechaFin();
+
+    // Métodos setters
+    void setTipo(string _tipo);
+    void setCosto(double _costo);
+    void setDuracionDias(int _duracionDias);
+    void setFechaInicio(string _fechaInicio);
+    void setFechaFin(string _fechaFin);
+
+    // Método para mostrar la información de la membresía
+    void mostrarInformacion();
+
+    // Método para modificar la membresía
+    void modificarMembresia(string nuevoTipo, double nuevoCosto, int nuevaDuracion, string nuevaFechaInicio, string nuevaFechaFin);
+
+};
+
+#endif // MEMBRESIA_H
